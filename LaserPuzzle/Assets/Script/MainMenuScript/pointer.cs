63ee6815131aa153997aec1102f1d71b9229215e
thwnd <sojungh@hknu.ac.kr>
2023-12-30 20:57:10 +0900

    레이저 시도
    미완

--- LaserPuzzle/Assets/Script/MainMenuScript/pointer.cs
+++ LaserPuzzle/Assets/Script/MainMenuScript/pointer.cs
@@ -2,24 +2,38 @@ using UnityEngine;
 
 public class LaserPointer : MonoBehaviour
 {
-    public LineRenderer laserLineRenderer;
+    public Transform laser;  // 레이저 오브젝트를 참조합니다.
+    public float speed = 5f;  // 레이저의 이동 속도를 설정합니다.
 
     void Start()
     {
-        // 레이저 포인터의 위치를 화면 중앙 아래로 설정
-        Vector3 screenCenterBottom = new Vector3(Screen.width / 2, 50);
-        transform.position = Camera.main.ScreenToWorldPoint(screenCenterBottom);
+        if (!laser)
+        {
+            Debug.LogError("Laser object not assigned.");
+            return;
+        }
     }
 
     void Update()
     {
-        // 마우스의 스크린 좌표를 월드 좌표로 변환
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = transform.position.z;
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        if (Camera.main == null)
+        {
+            Debug.LogError("Main camera not found.");
+            return;
+        }
 
-        // 레이저 빛의 시작점과 끝점을 설정
-        laserLineRenderer.SetPosition(0, transform.position);
-        laserLineRenderer.SetPosition(1, worldPosition);
+        // 마우스 위치를 가져옵니다.
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mouseWorldPosition.z = laser.position.z;  // 마우스 좌표의 z값은 레이저 오브젝트의 z값과 동일하게 설정합니다.
+
+        // 레이저의 길이를 마우스 위치에 따라 변경합니다.
+        Vector3 direction = mouseWorldPosition - laser.position;
+        float distance = direction.magnitude;
+        laser.localScale = new Vector3(laser.localScale.x, distance, laser.localScale.z);
+
+        // 레이저의 방향을 마우스 위치에 따라 변경합니다.
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        laser.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
