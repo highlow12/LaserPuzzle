d4bb2c56155e866e76b9bd51a69cd9b7a53ab197
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-01-24 22:11:26 +0900

    PORTAL FIX

--- LaserPuzzle/Assets/Script/ObjectScript/LaserPortalIn.cs
+++ LaserPuzzle/Assets/Script/ObjectScript/LaserPortalIn.cs
@@ -10,7 +10,11 @@ public class LaserPortalIn : LaserObject
     {
         if (connectedPortal != null)
         {
-            if((dir.normalized - transform.forward).normalized == Vector3.zero || (dir.normalized - transform.forward).normalized == Vector3.back)
+            //Debug.Log((dir.normalized - transform.forward).normalized);
+            if(
+                (dir.normalized - transform.forward).normalized == Vector3.zero 
+                || (dir.normalized - transform.forward).normalized== Vector3.right
+                || (dir.normalized - transform.forward).normalized == Vector3.right*-1)
             // 레이저가 이 포털에 충돌하면 연결된 포털로 레이저를 전송
                 connectedPortal.ReceiveLaser(dir);
         }
