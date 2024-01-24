401424d677a25a1523f12541c3a2d3282a9d8c7e
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-01-24 18:34:47 +0900

    laser animation2

--- LaserPuzzle/Assets/Script/ManagerScript/LaserManager.cs
+++ LaserPuzzle/Assets/Script/ManagerScript/LaserManager.cs
@@ -16,6 +16,9 @@ public class LaserManager : MonoBehaviour
     public float laserOffset = 0.5f;
 
     public float laserDuration = .5f;
+    public float LaserClearDelay = 3;
+
+    public Ease ease = Ease.Linear;
 
     public static LaserManager Instance
     {
@@ -71,14 +74,15 @@ public class LaserManager : MonoBehaviour
         //laser.DOMove(origin + dir * 0.5f + Vector3.up * laserOffset, laserDuration).SetSpeedBased();
 
         laser.localScale = Vector3.one*0.1f;
-        laser.DOScale(new Vector3(laser.localScale.x, laser.localScale.y, Mathf.Abs(i)), laserDuration).OnPlay
+        laser.DOScale(new Vector3(laser.localScale.x, laser.localScale.y, Mathf.Abs(i)), laserDuration).SetEase(Ease.Linear).OnPlay
             (
-                () => laser.DOMove(dir * 0.5f, laserDuration).SetRelative().OnComplete
+                () => laser.DOMove(dir * 0.5f, laserDuration).SetRelative().SetEase(Ease.Linear).OnComplete
                 (
                     () => {
                         if (hit.collider.TryGetComponent(out LaserObject _laser))
                         {
                             _laser.hitLaser(dir);
+                            StartCoroutine("IclearLasers",LaserClearDelay);
                         }
                     }
                 )
@@ -93,26 +97,41 @@ public class LaserManager : MonoBehaviour
     public void shootLaser(Vector3 origin, Vector3 target)
     {
         //Debug.Log(target);
-        
+
         var laser = Instantiate(Laser).transform;
         laser.SetParent(transform);
 
         var dir = target - origin;
 
-
-
-        laser.position = origin + dir * 0.5f + Vector3.up * laserOffset;
+        //laser.position = origin + dir * 0.5f + Vector3.up* laserOffset;
         //Debug.Log(laser.position);
         var i = dir.x + dir.z;
-        laser.localScale = new Vector3(laser.localScale.x, laser.localScale.y, Mathf.Abs(i));
+        //laser.localScale = new Vector3(laser.localScale.x, laser.localScale.y, Mathf.Abs(i));
 
-        dir = dir.normalized;
 
-        Vector3 angle = new(0, Vector3.SignedAngle(Vector3.forward, dir, Vector3.up), 0);
+
+        Vector3 angle = new(0, Vector3.SignedAngle(Vector3.forward, dir.normalized, Vector3.up), 0);
         //Debug.Log(angle);
 
         laser.localRotation = Quaternion.Euler(angle);
         //laser.gameObject.SetActive(false);
+
+        laser.position = origin + Vector3.up * laserOffset;
+        //laser.DOMove(origin + dir * 0.5f + Vector3.up * laserOffset, laserDuration).SetSpeedBased();
+
+        laser.localScale = Vector3.one * 0.1f;
+        laser.DOScale(new Vector3(laser.localScale.x, laser.localScale.y, Mathf.Abs(i)), laserDuration).SetEase(Ease.Linear).OnPlay
+            (
+                () => laser.DOMove(dir * 0.5f, laserDuration).SetRelative().SetEase(Ease.Linear).OnComplete
+                (
+                    () => 
+                            StartCoroutine("IclearLasers", LaserClearDelay))
+                        
+                    
+                
+            );
+
+
 
         lasers.Add(laser);
 
@@ -136,6 +155,25 @@ public class LaserManager : MonoBehaviour
         {
 
         }
+    }
+
+    public void LaserClear(float delay)
+    {
+        foreach (var t in lasers)
+        {
+            t.GetComponent<MeshRenderer>().material.DOFade(0, delay).SetEase(@ease).OnComplete(() => Destroy(t.gameObject));
+            //Destroy(t.gameObject);
+        }
+
+        lasers.Clear();
+    }
+
+    IEnumerator IclearLasers(float delay)
+    {
+        
+
+        LaserClear(delay);
+        yield return null;
     }
 
 }
--- LaserPuzzle/Assets/Script/ObjectScript/LaserPortalOut.cs
+++ LaserPuzzle/Assets/Script/ObjectScript/LaserPortalOut.cs
@@ -7,7 +7,7 @@ public class LaserPortalOut : LaserObject
     public void ReceiveLaser(Vector3 dir)
     {
         // LaserPortalIn에서 전송된 레이저를 받아 처리
-        shootLaser(transform.position, dir);
+        shootLaser(transform.position, transform.forward);
     }
 
     public override void hitLaser(Vector3 dir)
